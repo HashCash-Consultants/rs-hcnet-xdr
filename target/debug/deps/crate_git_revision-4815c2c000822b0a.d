bb9eef6c2807f8504b983a8e1230a9c753bb6b14
shantanu-hashcash <shantanu@hashcashconsultants.com>
2024-03-20 07:38:23 +0000

    target deleted

--- target/debug/deps/crate_git_revision-4815c2c000822b0a.d
+++
@@ -1,8 +0,0 @@
-/home/ubuntu/rs-hcnet-xdr/target/debug/deps/libcrate_git_revision-4815c2c000822b0a.rmeta: /home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/crate-git-revision-0.0.6/src/lib.rs /home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/crate-git-revision-0.0.6/src/test.rs
-
-/home/ubuntu/rs-hcnet-xdr/target/debug/deps/libcrate_git_revision-4815c2c000822b0a.rlib: /home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/crate-git-revision-0.0.6/src/lib.rs /home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/crate-git-revision-0.0.6/src/test.rs
-
-/home/ubuntu/rs-hcnet-xdr/target/debug/deps/crate_git_revision-4815c2c000822b0a.d: /home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/crate-git-revision-0.0.6/src/lib.rs /home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/crate-git-revision-0.0.6/src/test.rs
-
-/home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/crate-git-revision-0.0.6/src/lib.rs:
-/home/ubuntu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/crate-git-revision-0.0.6/src/test.rs:
